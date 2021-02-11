ac2d8b46cd7de69485778171d1f9f4dc52cd9180
Ali L <alil@alis-mbp-6.lan>
2021-02-11 11:46:32 -0800

    updated

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,15 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
 	console.log("User clicked on project " + idNumber);
+	$.get("http://localhost:3000/project/"+idNumber, callBackFn);
+}
+
+function callBackFn(result) {
+	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+	'</small></p></a>' + result['summary'];
+	$(`#project${result['id']} .details`).html(projectHTML);
 }
